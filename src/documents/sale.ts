d9db91e8a56de53ebdc17338bd799f293e196d7d
Viini <camperqueodeiacamper@gmail.com>
2022-09-23 23:30:15 -0300

    fix: ajustando documento de venda

--- src/documents/sale.ts
+++ src/documents/sale.ts
@@ -2,8 +2,9 @@ import { Document, ObjectId } from 'mongoose';
 import { ProductDocument } from './product';
 export interface SaleDocument extends Document {
   _id: ObjectId;
+  items: ProductDocument[];
   customer: ObjectId;
   date: Date;
+  cash_payment: boolean;
   total: number;
-  items: ProductDocument[];
 }
