bf6de65a4347ad57144b8208a9a7e1060f3eeb8f
Viini <camperqueodeiacamper@gmail.com>
2022-09-29 16:56:24 -0300

    chore: criando controller

--- src/company/controller/company.ts
+++ src/company/controller/company.ts
@@ -1,13 +1,19 @@
 import { Request, Response, Router } from 'express';
-import { generate as generateJwt } from '../../functions/jwt';
+import { UserNotFoundException } from '../../exceptions/user-not-found-exception';
+import {
+  generate as generateJwt,
+  validate as validateJwt
+} from '../../functions/jwt';
 import { AcessTokenException } from '../exceptions/acess-token-exception';
 import { CompanyEmailAlreadyExistsException } from '../exceptions/company-email-already-exists-exception';
 import { InvalidPayloadException } from '../exceptions/invalid-payload-exception';
+import { InvalidSearchParamsExeption } from '../exceptions/invalid-search-params';
 import { CompanyService } from '../service/company-service';
 import { CompanyCredentialsDto } from './dto/company-credentials-dto';
 import { CompanyRequestDto, toDocument } from './dto/company-request-dto';
 import { validateCompanyPayload } from './validations/company-creation';
 import { validateCompanyLoginPayload } from './validations/company-login';
+import { validateSearchParams } from './validations/company-search';
 export class CompanyController {
   private _router = Router();
   private companyService = new CompanyService();
@@ -19,6 +25,7 @@ export class CompanyController {
   constructor() {
     this._router.post('/', this.create);
     this._router.post('/login', this.login);
+    this._router.get('/:id', this.getById);
   }
 
   private create = async (req: Request, res: Response) => {
@@ -43,6 +50,33 @@ export class CompanyController {
         return;
       }
       res.status(500).send('Internal Server Error');
+    }
+  };
+
+  private getById = async (req: Request, res: Response) => {
+    const token = req.headers.authorization || '';
+    try {
+      validateJwt(token, async () => {
+        const id = req.params.id;
+
+        try {
+          validateSearchParams({ id });
+          const company = await this.companyService.findById(id);
+          res.status(200).send(company);
+        } catch (error) {
+          if (error instanceof UserNotFoundException) {
+            res.status(404).send(error.message);
+            return;
+          }
+          if (error instanceof InvalidSearchParamsExeption) {
+            res.status(400).send(error.message);
+            return;
+          }
+          res.status(500).send('Internal Server Error');
+        }
+      });
+    } catch (error) {
+      res.sendStatus(401);
     }
   };
 
