c1e0254b625d0bb0d3b87858587acfb64a6a3a3e
Luis Vinicius <luisvpersy@gmail.com>
2022-11-22 21:48:53 -0300

    chore: prettier format

--- src/company/controller/company-controller.test.ts
+++ src/company/controller/company-controller.test.ts
@@ -12,10 +12,12 @@ describe('Company Controller Tests', () => {
     new CompanyController();
     expect(ROUTER_MOCK.post).toHaveBeenCalledTimes(2);
     expect(ROUTER_MOCK.post).toHaveBeenCalledWith('/', expect.any(Function));
-    expect(ROUTER_MOCK.post).toHaveBeenCalledWith('/login', expect.any(Function));
+    expect(ROUTER_MOCK.post).toHaveBeenCalledWith(
+      '/login',
+      expect.any(Function)
+    );
 
     expect(ROUTER_MOCK.get).toHaveBeenCalledTimes(1);
     expect(ROUTER_MOCK.get).toHaveBeenCalledWith('/:id', expect.any(Function));
-  
-  })
-})
+  });
+});
